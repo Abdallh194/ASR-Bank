d7076a8ad0706e9ffc8d5724424e6b4b4fa61748
Abdallh <Abdallhsabry194@gmail.com>
2024-11-10 23:29:55 +0200

    enhance

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,18 +22,7 @@ const config = {
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
       },
-      keyframes: {
-        spotlight: {
-          "0%": {
-            opacity: 0,
-            transform: "translate(-72%, -62%) scale(0.5)",
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translate(-50%,-40%) scale(1)",
-          },
-        },
-      },
+
       colors: {
         fill: {
           1: "rgba(255, 255, 255, 0.10)",
@@ -102,23 +91,8 @@ const config = {
         inter: "var(--font-inter)",
         "ibm-plex-serif": "var(--font-ibm-plex-serif)",
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
   },
-  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
